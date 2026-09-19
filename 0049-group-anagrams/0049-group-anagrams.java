80b49e244225d6d7fb2cd81baecc85ea0536d95c
SARTHAK NAUTIYAL <nautiyalsarthak999@gmail.com>
2026-09-19 10:36:43 +0530

    Time: 6 ms (98.99%), Space: 49.1 MB (98.67%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = String.valueOf(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
